84335d421013498e5a710649338740c5e15bb3d1
SebastianLazarz <s.lazarz@hotmail.de>
2022-01-25 17:47:15 +0100

    deleted old project

--- Project_0_Banking_App_Experimental/src/main/java/com/revature/CustomerDao.java
+++
@@ -1,9 +0,0 @@
-package com.revature;
-
-import java.util.ArrayList;
-
-public interface CustomerDao {
-	public ArrayList<Customer> getallUsers();
-	public Customer getUser(String username);
-	public void addUser(Customer user);
-}
